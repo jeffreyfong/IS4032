4cabef2a31f197bb30e8146f24815006f6f39b32
Jeff <mr.jeffrey.f@outlook.com>
2017-02-09 10:34:24 +0800

    IS4032 Tutorial 3

--- app/src/main/java/jeff_fong/tutorial3/MainActivity.java
+++ app/src/main/java/jeff_fong/tutorial3/MainActivity.java
@@ -2,15 +2,21 @@ package jeff_fong.tutorial3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "MyFirstAppLogging";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        forceError();
+        //forceError();
+
+        Log.i(DEBUG_TAG, "In the onCreate() method of the Welcome App");
     }
 
     public void forceError() {
